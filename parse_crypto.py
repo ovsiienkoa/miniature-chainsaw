8ef248336881dd7cfa0d028f23b4d6b9b0b0c87f
ovsiienkoap <ovsiienko.andrii@gmail.com>
2025-07-22 22:11:41 +0300

    changes to crypto parse

--- parse_crypto.py
+++ parse_crypto.py
@@ -3,6 +3,20 @@ import io
 import os
 import requests
 
+def parse_crypto_directly(days = None):
+    link = "https://data.bitcoinity.org/export_data.csv?currency=USD&data_type=price_volume&r=day&t=lb&timespan=2y&vu=curr"
+    file = requests.get(link)
+    df = pd.read_csv(io.BytesIO(file.content))
+
+    if days is not None:
+        df = df[-days:]
+
+    df['date'] = pd.to_datetime(df['Time']).dt.date
+    df.rename(columns={'volume': 'number_sold'}, inplace=True)
+    df = df[['date', 'price', 'number_sold']]
+
+    return df
+
 if __name__ == '__main__':
 
     try:
@@ -10,12 +24,6 @@ if __name__ == '__main__':
     except:
         pass
 
-    link = "https://data.bitcoinity.org/export_data.csv?currency=USD&data_type=price_volume&r=day&t=lb&timespan=2y&vu=curr"
-    file = requests.get(link)
-    df = pd.read_csv(io.BytesIO(file.content))
-
-    df['date'] = pd.to_datetime(df['Time']).dt.date
-    df.rename(columns={'volume': 'number_sold'}, inplace=True)
-    df = df[['date', 'price', 'number_sold']]
+    df = parse_crypto_directly()
 
     df.to_csv("data/BTC.csv", index=False)
